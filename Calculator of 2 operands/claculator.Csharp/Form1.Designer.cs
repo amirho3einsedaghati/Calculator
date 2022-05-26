7566dd1d218b373554089d9d0c6c639490bea4be
amir hosein sedaghati <amirhosseinsedaghati42@gmail.com>
2022-05-26 19:56:47 +0430

    add Calculator of 2 operands

---
+++ Calculator of 2 operands/claculator.Csharp/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace claculator.Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btndiv = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.textNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(109, 170);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(109, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(224, 170);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(112, 23);
+            this.btnSubtract.TabIndex = 1;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(342, 170);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(107, 23);
+            this.btnMultiply.TabIndex = 2;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btndiv
+            // 
+            this.btndiv.Location = new System.Drawing.Point(455, 170);
+            this.btndiv.Name = "btndiv";
+            this.btndiv.Size = new System.Drawing.Size(110, 23);
+            this.btndiv.TabIndex = 3;
+            this.btndiv.Text = "/";
+            this.btndiv.UseVisualStyleBackColor = true;
+            this.btndiv.Click += new System.EventHandler(this.btndiv_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Location = new System.Drawing.Point(109, 238);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(456, 23);
+            this.btnEqual.TabIndex = 4;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // textNum
+            // 
+            this.textNum.Location = new System.Drawing.Point(109, 97);
+            this.textNum.Name = "textNum";
+            this.textNum.Size = new System.Drawing.Size(456, 20);
+            this.textNum.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 450);
+            this.Controls.Add(this.textNum);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.btndiv);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Caclulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btndiv;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.TextBox textNum;
+    }
+}
+
---
+++ Calculator of 2 operands/claculator.Csharp/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace claculator.Csharp
+{
+    public partial class Form1 : Form
+    {
+        int num1 = 0;
+        int num2 = 0;
+        int res;
+        char operators;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Converter(int res)
+        {
+            textNum.Text = res.ToString();
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            if (operators == '+' && textNum.Text != "")
+            {
+                num2 = int.Parse(textNum.Text);
+                res = num1 + num2;
+                Converter(res);
+            }
+
+            else if (operators == '-' && textNum.Text != "")
+            {
+                num2 = int.Parse(textNum.Text);
+                res = num1 - num2;
+                Converter(res);
+            }
+
+            else if (operators == '*' && textNum.Text != "")
+            {
+                num2 = int.Parse(textNum.Text);
+                res = num1 * num2;
+                Converter(res);
+            }
+
+            else if (operators == '/' && textNum.Text != "")
+            {
+                num2 = int.Parse(textNum.Text);
+                try
+                {
+                   res = num1 / num2;
+                   Converter(res);
+                }
+                   
+                catch(DivideByZeroException)
+                {
+                    MessageBox.Show("num2 have not to be a Zero");
+                }
+            }
+
+            else
+            {
+                res = 0;
+                Converter(res);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textNum.Text);
+            textNum.Clear();
+            operators = '+';
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textNum.Text);
+            textNum.Clear();
+            operators = '-';
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textNum.Text);
+            textNum.Clear();
+            operators = '*';
+        }
+
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textNum.Text);
+            textNum.Clear();
+            operators = '/';
+        }
+    }
+}
